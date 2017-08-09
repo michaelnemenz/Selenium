f91e703b8828e85a7b4210480c4cda319d6018aa
michael.nemenz <michael.nemenz@GSOD-DHAYES.orasi.com>
2017-08-09 10:36:14 -0400

    First Commit

---
+++ src/main/java/SeleniumTraining/SeleniumTraining/OpenBrowser.java
@@ -0,0 +1,16 @@
+package SeleniumTraining.SeleniumTraining;
+
+//System.setProperty("webdriver.chrome.driver", "C:\Users\michael.nemenz\Desktop\Driver\");
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenBrowser {
+	@Test
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();		
+		driver.get("https://the-internet.herokuapp.com/");		
+	}
+
+}
