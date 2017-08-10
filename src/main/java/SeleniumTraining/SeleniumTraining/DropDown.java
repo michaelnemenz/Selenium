baddb86cda7ab3620f0af4035d99820d6f228c9d
Michael Nemenz <michael.nemenz@orasi.com>
2017-08-10 09:03:04 -0400

    DropDown mofidifed

--- src/main/java/SeleniumTraining/SeleniumTraining/DropDown.java
+++ src/main/java/SeleniumTraining/SeleniumTraining/DropDown.java
@@ -3,13 +3,16 @@ package SeleniumTraining.SeleniumTraining;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class DropDown {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://the-internet.herokuapp.com/");
-		driver.findElement(By.linkText("Dropdown")).click();	
-		
+		driver.findElement(By.linkText("Dropdown")).click();		
+		driver.findElement(By.id("dropdown")).click();
+		Select dropdown = new Select(driver.findElement(By.id("dropdown")));   
+		dropdown.selectByVisibleText("Option 1");
 	}
 
 }
